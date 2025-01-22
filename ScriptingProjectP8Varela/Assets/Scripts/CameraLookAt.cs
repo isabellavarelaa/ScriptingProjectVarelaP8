09180850c2967c0eb4177d59d4f73cae8259eef3
isabellavarelaa <727144@student.dusd.net>
2025-01-22 14:15:48 -0800

    camera
    camera scripting

---
+++ ScriptingProjectP8Varela/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    public Transform target;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
